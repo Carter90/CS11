f27fb56b355f79b821a23b3bd7b4e2888270d71b
Carter Frost <carter90@gmaill.com>
2018-06-21 17:37:12 -0700

    Added leading 0 to digits < 10 for the weeks directorys

--- BashScripts.sh
+++ BashScripts.sh
@@ -1,2 +1,2 @@
 #Create the directory structure
-for i in $(seq 1 15); do mkdir "Week $i" && touch ./"Week $i"/notes.txt; done
+for i in {01..15}; do mkdir "Week $i" && touch ./"Week $i"/notes.txt; done
