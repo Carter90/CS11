504277f5b9a80c2f277006ae68a6e6ea26b9663b
Carter Frost <carter90@gmaill.com>
2018-09-05 16:35:02 -0700

    Added blank.cpp and the notes from the section

---
+++ Week 01/blank.cpp
@@ -0,0 +1,30 @@
+/** 
+ * Name: Carter Frost 
+ * Email: Carter90@gmail.com
+ * Filename: .cpp
+ * Example name: 
+ * Description: 
+ * Code status: Broke dont run me
+ * Program compile: 
+ * Errors or warnings: 
+ * Comments: 
+ */
+#include <iostream>
+using namespace std;
+
+	/**
+	 * main
+	 * prints Greetings!
+	 * @params Ignored
+	 * @returns 0 compleated sucessfully
+	 */	
+int main(int argc, char *argv[]) { 
+cout << "Greetings!" << endl; 
+return(0); 
+
+}
+/*
+int main() {
+	cout << "Greetings!\n" << endl; //this prints out "Greetings!" and a new line
+} //end main
+*/
