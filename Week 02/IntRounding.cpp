09be2aff317fb4ff3c125bd87b4287fe5f0e1166
Carter Frost <Carter90@gmail.com>
2018-09-12 18:36:20 -0700

    Example from the session for Integer rounding

---
+++ Week 02/IntRounding.cpp
@@ -0,0 +1,14 @@
+#include <cmath>  
+#include <iostream>
+using namespace std;
+int main ()
+{
+  double x = 2.3;
+  //think of the graph of y = x its a line right
+  cout << "x:" << x << endl; 
+  // what about y = (int)x it's like a stair case
+  cout << "(int)x:" << (int)x << endl; 
+  // what about y = ceil(x) it's like a stair case but shifted
+  cout << "ceil(x):" << ceil(x) << endl; 
+  return 0;
+}
