a7a39396033bce7df3fde2967ea466e0229cca19
Carter Frost <carter90@gmaill.com>
2018-10-31 20:43:56 -0700

    Fixed BashScript to reflect current structure

--- BashScripts.sh
+++ BashScripts.sh
@@ -1,2 +1,2 @@
 #Create the directory structure
-for i in {01..15}; do mkdir "Week $i" && touch ./"Week $i"/notes.txt; done
+for i in {01..15}; do mkdir "Week_$i" && touch ./"Week_$i"/notes.txt; done
