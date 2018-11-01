51eae4a4ed695ca6b6ec7fc56c0bfad7958c016d
Carter Frost <carter90@gmaill.com>
2018-10-31 18:57:00 -0700

    Added file for Example Starter Code

---
+++ blank.cpp
@@ -0,0 +1,25 @@
+/** 
+ * @author: Carter Frost 
+ * Email: cs11@carterfrost.com
+ * Filename: blank.cpp
+ * Example name: Example Starter Code
+ * Description: This is the file to start with when building examples
+ * Code status: Working!
+ * Program compile: Yes
+ * Errors or warnings: None
+ * Comments: 
+ */
+#include <iostream>
+using namespace std;
+
+	/**
+	 * main
+	 * prints Greetings!
+	 * @params Ignored
+	 * @returns 0 compleated sucessfully
+	 */	
+int main(int argc, char *argv[]) { 
+	cout << "Greetings!" << endl; 
+	return(0); 
+} //end main
+
