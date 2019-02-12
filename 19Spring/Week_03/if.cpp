ebaccef625d0eb0aca84ac17eaf825bd3b61ed83
Carter Frost <carter90@gmail.com>
2019-02-11 18:39:57 -0800

    if.cpp: in session example of conditionals

---
+++ 19Spring/Week_03/if.cpp
@@ -0,0 +1,40 @@
+/** 
+ * @author: Carter Frost 
+ * Email: cs11@carterfrost.com
+ * Filename: if.cpp
+ * Example name: Conditionals
+ * Description: use of if and else
+ * Code status: Working!
+ * Program compile: Yes
+ * Errors or warnings: None
+ * Comments: 
+ */
+#include <iostream>
+using namespace std;
+
+	/**
+	 * main
+	 * currently prints "dog is big yeah!"
+	 * @params Ignored
+	 * @returns 0 compleated sucessfully
+	 */	
+int main(int argc, char *argv[]) {
+	bool is_big = true;
+	int t = 31;
+	if (t > 50) { 
+		cout << "yo";
+	} //end if t > 50
+	else if (t > 20){ 
+		cout << "dog";
+	} //end else if t > 20
+	else { // if t < 20 same as `else if (t <= 20){`
+		cout << "smallz";
+	} //end else t < 20 
+	
+	is_big = (t > 30); 
+	//This if is seprate from the above ladder
+	if (is_big){cout << " is big yeah!";}  
+	cout << endl;
+	return(0); 
+} //end main
+
